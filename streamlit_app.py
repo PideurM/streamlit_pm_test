aeb8b51701f2c30aef5e73711bc536ad8eac4ad9
Pierre MULLER <60134228+PideurM@users.noreply.github.com>
2023-05-12 16:30:42 +0200

    test 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
-st.write("Hello world!")
+st.header('Breakfast Menu')
 
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
+st.text('🍌🍞 Banana Bread')
+st.text('🍳🥓🍞 Full English Breakfast')
+st.text('🥞🍓🍌 Pancakes with Berries & Banana')
+st.text('🍳🥓🍞 Eggs & Bacon')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
