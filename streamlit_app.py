c6597ab81349cbd35da239175186386e08f0ba2e
Pierre MULLER <60134228+PideurM@users.noreply.github.com>
2023-05-15 17:16:27 +0200

    Add snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import requests
+
+
 
 st.header('Breakfast Menu')
 
@@ -12,3 +16,22 @@ st.text('🥞🍓🍌 Pancakes with Berries & Banana')
 st.text('🍳🥓🍞 Eggs & Bacon')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+df = pd.read_csv('fruit_macro.csv')
+df = df.set_index('Fruit')
+
+fruit_selected = st.multiselect('Select your fruit', list(df.index),['Avocado','Banana'])
+fruit_to_show = df.loc[fruit_selected]
+
+#Display the dataframe
+st.dataframe(fruit_to_show)
+
+# Call fruityvice API
+st.header('Fruityvice Fruit Advice !')
+fruit_choice = st.text_input('What fruit do you want advice on?','Kiwi')
+st.write('You selected', fruit_choice)
+fruityvice_response = requests.get('https://www.fruityvice.com/api/fruit/' + fruit_choice)
+fruityvice_json = pd.json_normalize(fruityvice_response.json())
+st.dataframe(fruityvice_json) # Display the dataframe
+
+
